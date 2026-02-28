64edf4f7f81aae4b37ca0b0b5a54d078753e0ebb
Mohamed Amine <nafiai.medamine@gmail.com>
2026-02-28 03:09:55 +0000

    feat(migrations): update colocations schema with owner and status

--- database/migrations/2026_02_24_122818_create_colocations_table.php
+++ database/migrations/2026_02_24_122818_create_colocations_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,8 +14,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description')->nullable();
-            $table->foreignId('admin_id')->constrained('users')->onDelete('cascade');
-            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
+            $table->enum('status', [
+                'active',
+                'cancelled'
+            ])->default('active');
             $table->timestamps();
         });
     }
